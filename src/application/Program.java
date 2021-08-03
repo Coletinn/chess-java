0054ae5954834cf02d15ac3f07a755bd7d27c5ae
Coletinn <gustavocoleto@hotmail.com>
2021-08-03 00:11:26 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Position pos = new Position(8, 8);
+		System.out.println(pos);
 	}
 
 }
